6823c1c2760a833ec6ba2a55b5e20350c621cf4b
Daniel Mies <daniel.mies@codecentric.de>
2020-12-16 20:02:59 +0100

    refactoring: generating the christmas tree

--- tannenbaum.go
+++ tannenbaum.go
@@ -1,13 +1,23 @@
 package main
 
 func PrintTannenbaum(numberOfLines int) (tannenbaum string) {
-	switch numberOfLines {
-	case 1:
-		tannenbaum = "*"
-	case 2:
-		tannenbaum = " * \n***"
-	case 3:
-		tannenbaum = "  *  \n *** \n*****"
+
+	for i := 1; i <= numberOfLines; i++ {
+		// numberOfLines - i
+		leerzeichen := ""
+		for k := 1; k <= numberOfLines-i; k++ {
+			leerzeichen += " "
+		}
+		// Zeile (i)
+		sternchen := ""
+		// Sternchen i*2-1
+		for j := 1; j <= i*2-1; j++ {
+			sternchen += "*"
+		}
+		tannenbaum += leerzeichen + sternchen + leerzeichen
+		if i < numberOfLines {
+			tannenbaum += "\n"
+		}
 	}
-	return tannenbaum
+	return
 }
