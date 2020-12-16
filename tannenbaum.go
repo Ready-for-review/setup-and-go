9dc52f34b837de8762ce48f86b775d58c5815e1a
Daniel Mies <daniel.mies@codecentric.de>
2020-12-16 19:46:30 +0100

    support for 3 lines + refactoring

--- tannenbaum.go
+++ tannenbaum.go
@@ -1,9 +1,13 @@
 package main
 
-func PrintTannenbaum(numberOfLines int) string {
-	if numberOfLines == 1 {
-		return "*"
-	} else {
-		return " * \n***"
+func PrintTannenbaum(numberOfLines int) (tannenbaum string) {
+	switch numberOfLines {
+	case 1:
+		tannenbaum = "*"
+	case 2:
+		tannenbaum = " * \n***"
+	case 3:
+		tannenbaum = "  *  \n *** \n*****"
 	}
+	return tannenbaum
 }
--- tannenbaum_test.go
+++ tannenbaum_test.go
@@ -17,9 +17,14 @@ func Test_PrintTannenbaum(t *testing.T) {
 		want := "*"
 		assertEquals(got, want, t)
 	})
-	t.Run("a not so small tannenbaum with 2 line", func(t *testing.T) {
+	t.Run("a not so small tannenbaum with 2 lines", func(t *testing.T) {
 		got := PrintTannenbaum(2)
 		want := " * \n***"
+		assertEquals(got, want, t)
+	})
+	t.Run("a tannenbaum with 3 lines", func(t *testing.T) {
+		got := PrintTannenbaum(3)
+		want := "  *  \n *** \n*****"
 		assertEquals(got, want, t)
 	})
 }
